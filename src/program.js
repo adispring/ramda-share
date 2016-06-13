0c7ba53cd8d65388ba73a5162825f7a56f635135
wangzengdi <wangzengdi@gmail.com>
2016-06-13 10:19:00 +0800

    add rxjs

--- src/program.js
+++ src/program.js
@@ -1,8 +1,9 @@
 'use strict';
-var curry = require('lodash').curry;
+// var curry = require('lodash').curry;
+import R from 'ramda';
+import S from 'sanctuary';
+import L from 'lodash';
 
-var match = curry(function(what, str) {
-  return str.match(what);
-});
+const match = L.curry((what, str) => str.match(what));
 
 console.log(match(/\s+/g, "hello   world"));
