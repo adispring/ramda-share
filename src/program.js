8275275bb9abc57ebf4fca3c7e91791da4eac4e0
wangzengdi <wangzengdi@gmail.com>
2016-07-26 07:13:54 +0800

    develop add 2 console.log to test ramda

--- src/program.js
+++ src/program.js
@@ -7,3 +7,9 @@ import L from 'lodash';
 const match = L.curry((what, str) => str.match(what));
 
 console.log(match(/\s+/g, "hello   world"));
+
+console.log(S.Left('Cannot divede by zero'));
+
+console.log(S.Right([1, 2, 3]).inspect());
+
+
