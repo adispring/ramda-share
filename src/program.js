a1689d6dff0b1f93310f27eac4b7d1de96c88854
wangzengdi <sunnyadi@163.com>
2016-07-30 11:54:16 +0800

    split memoize to memoize.js

--- src/program.js
+++ src/program.js
@@ -1,37 +1,3 @@
 // import R from 'ramda';
+import { memoize } from './memoize';
 
-const memoize = f => {
-  const cache = {};
-  return function() {
-    const argStr = JSON.stringify(arguments);
-    cache[argStr] = cache[argStr] || f.apply(f, arguments);
-    return cache[argStr];
-  };
-};
-
-let count = 0;
-const sqrt = memoize(x => {
-  count += 1;
-  return x * x;
-});
-
-sqrt(3);
-sqrt(3);
-sqrt(3);
-
-console.log(`count: ${count}`);
-
-sqrt(4);
-sqrt(5);
-
-console.log(`count: ${count}`);
-
-// 箭头函数不会在其内部暴露出  arguments 对象
-// const memoizeArrow = f => {
-//   const cache = {};
-//   return (...args) => {
-//     const argStr = JSON.stringify(args);
-//     cache[argStr] = cache[argStr] || f.apply(f, args);
-//     return cache[argStr];
-//   };
-// };
