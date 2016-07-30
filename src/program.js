72928872f536d04552418cace405d8980002f72d
wangzengdi <sunnyadi@163.com>
2016-07-30 20:57:06 +0800

    **** High-order Function Benefit: business example ****

--- src/program.js
+++ src/program.js
@@ -1,3 +1,4 @@
 // import R from 'ramda';
 // import { memoize } from './memoize';
-import { curry } from './curry';
+// import { curry } from './curry';
+import { business } from './business';
---
+++ test/mock/biz.js
@@ -0,0 +1,37 @@
+export const bizList = [
+  {
+    name: 'xiaowangfu',
+    id: 1,
+    sale: 200,
+  },
+  {
+    name: 'dawangfu',
+    id: 3,
+    sale: 700,
+  },
+  {
+    name: 'kfc',
+    id: 6,
+    sale: 900,
+  },
+  {
+    name: 'motoroLa',
+    id: 9,
+    sale: 600,
+  },
+  {
+    name: 'mCdOnalD',
+    id: 13,
+    sale: 1700,
+  },
+  {
+    name: 'Kingburg',
+    id: 15,
+    sale: 100,
+  },
+  {
+    name: 'Zhangliang',
+    id: 18,
+    sale: 100,
+  },
+];
