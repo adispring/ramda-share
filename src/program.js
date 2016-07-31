206d3e090df8e291d4ab49baaf6eb5f4c8ed5c70
wangzengdi <sunnyadi@163.com>
2016-07-31 20:21:15 +0800

    **** map ****

--- src/program.js
+++ src/program.js
@@ -1,5 +1,6 @@
 // import R from 'ramda';
 // import { memoize } from './memoize';
 // import { curry } from './curry';
-import { business } from './business';
+// import { business } from './business';
 // import { cubesum } from './cubesum';
+import { map } from './map';
